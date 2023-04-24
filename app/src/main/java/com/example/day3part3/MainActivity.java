420f1a3b60335fbef7f48cc9d7cb4a9c7724c7c4
sniwas <sniwaserode@gmail.com>
2023-04-24 23:40:10 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/day3part3/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.day3part3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        },6000);
+    }
+}
